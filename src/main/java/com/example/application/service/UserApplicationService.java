5ac832464e45899a2936359fa35f1545e65e2c57
shoji yuki <youkey2929@gmail.com>
2025-09-27 23:30:44 +0900

    6.1.2 メッセージプロパティ

--- src/main/java/com/example/application/service/UserApplicationService.java
+++ src/main/java/com/example/application/service/UserApplicationService.java
@@ -1,21 +1,31 @@
 package com.example.application.service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserApplicationService {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	/** 性別のMapを生成する */
 	public Map<String, Integer> getGenderMap() {
 		// キーがString型でバリューがInteger型のMapを生成
 		Map<String, Integer> genderMap = new LinkedHashMap<>();
-		// 変数genderMapにキーが男性、バリューが1のデータを追加
-		genderMap.put("男性", 1);
-		// 変数genderMapにキーが女性、バリューが2のデータを追加
-		genderMap.put("女性", 2);
+		// messagePropertiesのキーmaleのバリューを取得
+		String male = messageSource.getMessage("male", null, Locale.JAPAN);
+		// messagePropertiesのキーfemaleのバリューを取得
+		String female = messageSource.getMessage("female", null, Locale.JAPAN);
+		// 変数genderMapにキーがmale、バリューが1のデータを追加
+		genderMap.put(male, 1);
+		// 変数genderMapにキーがfemale、バリューが2のデータを追加
+		genderMap.put(female, 2);
 		// 呼び出し元にgenderMapを返す
 		return genderMap;
 	}
